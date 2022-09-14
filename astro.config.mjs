fb3972790ec34d783f35b0788d9cae1925d55988
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-09-14 17:57:15 +0200

    -

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  output: "server",
+  integrations: [react(), mdx()],
+  output: 'server',
   adapter: vercel()
 });
